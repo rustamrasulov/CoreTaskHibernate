522bc976f78855d2d47bd92083ac8d5e1307adf0
rustamrasulov <rustam.rasulov@miirrr.com>
2022-03-03 21:45:55 +0500

    v0.3 Исправление замечаний ментора в кодстайле.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -20,13 +20,13 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS users" +
-                    "                    (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
-                    "                    name VARCHAR (100)," +
-                    "                    lastName VARCHAR (100)," +
-                    "                    age INT)")
+                            "                    (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+                            "                    name VARCHAR (100)," +
+                            "                    lastName VARCHAR (100)," +
+                            "                    age INT)")
                     .executeUpdate();
             session.getTransaction().commit();
 
@@ -39,7 +39,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
             session.getTransaction().commit();
@@ -52,16 +52,10 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             User user = new User(name, lastName, age);
             session.beginTransaction();
             session.persist(user);
-/*          SQLQuery insertQuery =  session.createSQLQuery("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)");
-            insertQuery.setParameter(1, name);
-            insertQuery.setParameter(2, lastName);
-            insertQuery.setParameter(3, age);
-            insertQuery.executeUpdate();
-*/
             session.getTransaction().commit();
         } finally {
             if (session != null) {
@@ -72,16 +66,14 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        try(Session session = Util.getSessionFactory().openSession()) {
-        session.beginTransaction();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
 
-        User user = session.get(User.class, id);
-        if (user != null) {
+            User user = session.get(User.class, id);
             SQLQuery removeQuery = session.createSQLQuery("DELETE FROM users WHERE id = ?");
             removeQuery.setParameter(1, id);
             removeQuery.executeUpdate();
-        }
-        session.getTransaction().commit();
+            session.getTransaction().commit();
         } finally {
             if (session != null) {
                 session.close();
@@ -91,7 +83,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             return session.createQuery("from User", User.class).list();
         } finally {
             if (session != null) {
@@ -102,7 +94,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try(Session session = Util.getSessionFactory().openSession()) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             session.beginTransaction();
             session.createSQLQuery("DELETE FROM users").executeUpdate();
             session.getTransaction().commit();
